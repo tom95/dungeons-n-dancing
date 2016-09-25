119baf697beea884e79b782081f668dcd2c458c9
Corinna Jaschek <corinna.jaschek@gmail.com>
2016-09-25 23:14:23 +0200

    added a randomPlaylist class for random playback

---
+++ src/app/random-playlist.spec.ts
@@ -0,0 +1,10 @@
+/* tslint:disable:no-unused-variable */
+
+import { addProviders, async, inject } from '@angular/core/testing';
+import {RandomPlaylist} from './random-playlist';
+
+describe('RandomPlaylist', () => {
+  it('should create an instance', () => {
+    expect(new RandomPlaylist()).toBeTruthy();
+  });
+});
