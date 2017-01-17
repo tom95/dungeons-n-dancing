8395f8bc54c3caaa7c4e10dd57c250da92e01ceb
Tom Beckmann <tomjonabc@gmail.com>
2017-01-17 18:25:30 +0100

    Add a couple sad tests

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,11 +2,11 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
 import { AppComponent } from './app.component';
 import { DndTrackButtonComponent } from './dnd-track-button/dnd-track-button.component';
 import { PlaylistSelectorComponent } from './playlist-selector/playlist-selector.component';
-import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { TrackChooserYoutubeComponent } from './track-chooser-youtube/track-chooser-youtube.component';
 import { TrackChooserLocalComponent } from './track-chooser-local/track-chooser-local.component';
 import {
--- src/app/dnd-track-button/dnd-track-button.component.spec.ts
+++ src/app/dnd-track-button/dnd-track-button.component.spec.ts
@@ -1,11 +1,85 @@
 /* tslint:disable:no-unused-variable */
 
-import { TestBed, async } from '@angular/core/testing';
+import {
+  async,
+  TestBed,
+  DebugElement,
+  ComponentFixture,
+  ComponentFixtureAutoDetect
+} from '@angular/core/testing';
+import { By } from '@angular/platform-browser';
+
+import { Component, Input, Output, ViewChild, EventEmitter } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { DndTrackButtonComponent } from './dnd-track-button.component';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { DropboxTrack } from '../dropbox-track';
+
+@Component({ selector: 'dnd-track-chooser-local', template: '' }) class TestLocalComponent {
+  @Input() track;
+  @Output() created = new EventEmitter();
+}
+@Component({ selector: 'dnd-track-chooser-deezer', template: '' }) class TestDeezerComponent {
+  @Input() track;
+  @Output() created = new EventEmitter();
+}
+@Component({ selector: 'dnd-track-chooser-youtube', template: '' }) class TestYoutubeComponent {
+  @Input() track;
+  @Output() created = new EventEmitter();
+}
+@Component({ selector: 'dnd-track-chooser-dropbox', template: '' }) class TestDropboxComponent {
+  @Input() track;
+  @Output() created = new EventEmitter();
+}
+@Component({ template: '<dnd-track-button></dnd-track-button><template ngbModalContainer></template>' })
+class TestHostComponent {
+  @ViewChild(DndTrackButtonComponent) trackButtonComponent: any;
+}
 
 describe('Component: DndTrackButton', () => {
+  let comp: DndTrackButtonComponent;
+  let fixture: ComponentFixture<TestHostComponent>;
+  let openButton: DebugElement;
+
+  beforeEach(() => {
+    TestBed.configureTestingModule({
+      declarations: [
+        DndTrackButtonComponent,
+        TestHostComponent,
+        TestLocalComponent,
+        TestDeezerComponent,
+        TestYoutubeComponent,
+        TestDropboxComponent
+      ],
+      imports: [
+        NgbModule.forRoot(),
+        FormsModule
+      ],
+      providers: [
+      ]
+    )
+    .compileComponents();
+
+    fixture = TestBed.createComponent(TestHostComponent);
+    comp = fixture.componentInstance.trackButtonComponent;
+    comp.track = new DropboxTrack('Test Title', 'http://blah');
+    fixture.detectChanges();
+
+    openButton = fixture.debugElement.query(By.css('.btn'));
+  });
+
   it('should create an instance', () => {
-    let component = new DndTrackButtonComponent();
-    expect(component).toBeTruthy();
+    expect(comp).toBeTruthy();
+  });
+
+  it('should display its track\'s title', () => {
+    expect(fixture.debugElement.query(By.css('.track-title')).nativeElement.textContent).toEqual('Test Title');
+  });
+
+  it('should open the chooser modal', () => {
+    expect(fixture.debugElement.query(By.css('.modal'))).toBeNull();
+    openButton.triggerEventHandler('click', null);
+    fixture.detectChanges();
+    expect(fixture.debugElement.query(By.css('.modal'))).not.toBeNull();
   });
 });
